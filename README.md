011dbad394b76ea1526010d2c086f359288d2d07
Binder <bind_mc@lx501.pa.op.dlr.de>
2023-12-13 03:14:48 +0100

    tmp and filter plot and telma

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# coral-visualizations
+# MA-lidar-visualizations
+
+- Visualize files in output folder
+- scp command to server: scp -r -P 181 filter1 bind_mc@extern05.pa.op.dlr.de:/var/www/html/ma-lidar-calendar/plots/coral/
+- files are only named with date + starting time + duration
--- plot_lidar_tmp.py
+++ plot_lidar_tmp.py
@@ -29,7 +29,7 @@ import filter, cmaps
 plt.style.use('latex_default.mplstyle')
 
 
-def plot_lidar_filter(CONFIG_FILE):
+def plot_lidar_tmp(CONFIG_FILE):
     """Visualize lidar measurements (time-height diagrams + absolute temperature measurements)"""
     
     """Settings"""
@@ -41,7 +41,7 @@ def plot_lidar_filter(CONFIG_FILE):
     else:
         obs_list = os.path.join(config.get("INPUT","OBS_FOLDER"), config.get("INPUT","OBS_FILE"))
     
-    os.makedirs(os.path.join(config.get("OUTPUT","FOLDER"),"filter1"), exist_ok=True)
+    os.makedirs(os.path.join(config.get("OUTPUT","FOLDER"),'tmp'), exist_ok=True)
     zrange = eval(config.get("GENERAL","ALTITUDE_RANGE"))
     trange = eval(config.get("GENERAL","TRANGE"))
     
@@ -123,35 +123,37 @@ def plot_lidar_filter(CONFIG_FILE):
         vert_res       = (ds['alt_plot'][1]-ds['alt_plot'][0]).values[0]
         tprime_bwf20, tbg20 = filter.butterworth_filter(ds["temperature"].values, highcut=1/20, fs=1/vert_res, order=5, mode='low')
         tprime_bwf15, tbg15 = filter.butterworth_filter(ds["temperature"].values, highcut=1/15, fs=1/vert_res, order=5, mode='low')
+        tprime_bwf15, _ = filter.butterworth_filter(ds["temperature"].values, highcut=1/15, fs=1/vert_res, order=5, mode='high')
         #ds['tprime_bwf20'] = butterworthf(ds, highcut=1/20, fs=1/0.1, order=5, single_column_filter=True)['tmp_pert']
         #tprime_bwf15, tbg15 = butterworthf(ds, highcut=1/15, fs=1/0.1, order=5, single_column_filter=True)['tmp_pert']
         #meanT = ds["temperature"].mean(dim='time')
         tprime_temp  = (ds["temperature"]-ds["temperature"].mean(dim='time')).values
 
-        vars = [tprime_temp, tprime_bwf15, tprime_bwf20]
+        vars = [ds["temperature"].values, tbg15, tprime_bwf15]
         """Figure"""
-        gskw = {'hspace':0.04, 'wspace':0.03, 'width_ratios': [4,2], 'height_ratios': [5,5,5,1]} #  , 'width_ratios': [5,5]}
+        gskw = {'hspace':0.04, 'wspace':0.03, 'width_ratios': [4,2], 'height_ratios': [5,1,5,1]} #  , 'width_ratios': [5,5]}
         fig, axes = plt.subplots(4,2, figsize=(7,12), sharey=True, gridspec_kw=gskw)
+        axes[1,0].axis('off')
+        axes[1,1].axis('off')
         axes[3,0].axis('off')
         axes[3,1].axis('off')
 
         h_fmt      = mdates.DateFormatter('%H')
         h_interv   = mdates.HourLocator(interval = 2)
-        filter_str =['Filter: Temp-mean','Filter: 15km-BW','Filter: 20km-BW']
-        for k in range(0,3):
+        filter_str =["Temperature", "15km-BW lowpass","15km-BW highpass"]
+        for k in [0,2]:
             ax_lid = axes[k,0]
             ax0    = axes[k,1]
             if k==0:
-                clev = [-32,-16,-8,-4,-2,-1,-0.5,0.5,1,2,4,8,16,32] # 32
-                clev_l = [-16,-4,-1,1,4,16]
-                cmap = cmaps.get_wave_cmap()
-            elif k==1:
-                clev = [-32,-16,-8,-4,-2,-1,-0.5,0.5,1,2,4,8,16,32] # 32
-                clev_l = [-16,-4,-1,1,4,16]
-                cmap = cmaps.get_wave_cmap()
+                cb_range  = eval(config.get("GENERAL", "TRANGE"))
+                clev      = np.arange(cb_range[0],cb_range[1],10)
+                clev_l    = np.arange(cb_range[0]+10,cb_range[1]-10,20)
+                cbar_l    = "temperature / K" 
+                cmap      = plt.get_cmap('turbo')
             elif k==2:
                 clev = [-32,-16,-8,-4,-2,-1,-0.5,0.5,1,2,4,8,16,32] # 32
                 clev_l = [-16,-4,-1,1,4,16]
+                cbar_l    = "T' / K" 
                 cmap = cmaps.get_wave_cmap()
 
             norm = BoundaryNorm(boundaries=clev, ncolors=cmap.N, clip=True)
@@ -165,13 +167,10 @@ def plot_lidar_filter(CONFIG_FILE):
             ax_lid.yaxis.set_minor_locator(AutoMinorLocator()) 
             ax_lid.xaxis.set_minor_locator(AutoMinorLocator())
             ax_lid.xaxis.set_label_position('top')
-            if k==0:
-                ax_lid.tick_params(which='both', labelbottom=False,labeltop=True)
-            else:
-                ax_lid.tick_params(which='both', labelbottom=False,labeltop=False)
+            ax_lid.tick_params(which='both', labelbottom=False,labeltop=True)            
             ax_lid.set_ylabel('altitude / km')
 
-            ypp = 0.96
+            ypp = 0.965
             ax_lid.text(0.03, ypp, filter_str[k], transform=ax_lid.transAxes, verticalalignment='top', bbox={"boxstyle" : "round", "lw":0.67, "facecolor":"white", "edgecolor":"black"})
             # if k==0:
             #     info_str = ""
@@ -181,53 +180,54 @@ def plot_lidar_filter(CONFIG_FILE):
             # ---- T-axis ---- #
             lw_thin=0.1
             lw_thick=2
-
-            ax1 = ax0.twiny()
-            ax1.axvline(x=0,c='grey',lw=lw_thick-1)
             trange_prof = eval(config.get("GENERAL", "TRANGE_PROF"))
             ax0.set_xlim(trange_prof[0],trange_prof[1])
-            ax1.set_xlim([-49.5,25])
-
             ax0.yaxis.set_minor_locator(AutoMinorLocator()) 
             ax0.xaxis.set_minor_locator(AutoMinorLocator())
-            ax1.xaxis.set_minor_locator(AutoMinorLocator())
-            
             ax0.set_ylabel('altitude / km')
             ax0.yaxis.set_label_position("right")
             ax0.xaxis.set_label_position('top')
             ax0.yaxis.tick_right()
             ax0.tick_params(which='both', labelbottom=False, labeltop=True, labelright=True, left=True, top=True, bottom=False)
-            
-            ax1.tick_params(which='both', axis='x', bottom=True, top=False, labeltop=False, labelbottom=True, colors='red')
-            ax1.spines['bottom'].set_color('red')
-            ax1.xaxis.set_label_position('bottom')
+
+            if k==2:
+                ax1 = ax0.twiny()
+                ax1.axvline(x=0,c='grey',lw=lw_thick-1)
+                ax1.set_xlim([-49.5,25])
+                ax1.xaxis.set_minor_locator(AutoMinorLocator())
+                ax1.tick_params(which='both', axis='x', bottom=True, top=False, labeltop=False, labelbottom=True, colors='red')
+                ax1.spines['bottom'].set_color('red')
+                ax1.xaxis.set_label_position('bottom')
 
             if k==0:
                 ax0.set_xlabel('temperature / K')
+                ax0.tick_params(which='both', bottom=True)
             else:
-                ax0.tick_params(labeltop=False)
+                ax0.tick_params(which='both',labeltop=True,top=True,bottom=False,labelbottom=False)
             if k==2:
                 ax1.set_xlabel("T' / K", color='red')
-            else:
-                ax1.tick_params(labelbottom=False, colors='red')
             
             for t in range(0,np.shape(ds['temperature'])[0],4):      
                 ax0.plot(ds["temperature"][t],ds['alt_plot'],lw=lw_thin,color='black')
-                ax1.plot(vars[k][t],ds['alt_plot'],lw=lw_thin,color='red')
+                if k==2:
+                    ax1.plot(vars[k][t],ds['alt_plot'],lw=lw_thin,color='red')
             ax0.plot(np.mean(ds["temperature"],axis=0),ds['alt_plot'],lw=lw_thick,color='black')
-            ax1.plot(np.nanmean(vars[k],axis=0),ds['alt_plot'], lw=lw_thick, color='red')
+            if k==2:
+                ax1.plot(np.nanmean(vars[k],axis=0),ds['alt_plot'], lw=lw_thick, color='red')
             ax0.grid()
 
-            numb_str = ['a','b','c','d','e','f','g','h']
+            numb_str = ['a','b','c','d','c','d']
             xpp0 = 0.95
             xpp1 = 0.92
             ax_lid.text(xpp0, ypp, numb_str[2*k], verticalalignment='top', horizontalalignment='right', transform=ax_lid.transAxes, weight='bold', bbox={"boxstyle" : "circle", "lw":0.67, "facecolor":"white", "edgecolor":"black"})
-            ax1.text(xpp1, ypp, numb_str[2*k+1], verticalalignment='top', horizontalalignment='right', transform=ax1.transAxes, weight='bold', bbox={"boxstyle" : "circle", "lw":0.67, "facecolor":"white", "edgecolor":"black"})
-      
+            if k==2:
+                ax1.text(xpp1, ypp, numb_str[2*k+1], verticalalignment='top', horizontalalignment='right', transform=ax1.transAxes, weight='bold', bbox={"boxstyle" : "circle", "lw":0.67, "facecolor":"white", "edgecolor":"black"})
+            else:
+                ax0.text(xpp1, ypp, numb_str[2*k+1], verticalalignment='top', horizontalalignment='right', transform=ax0.transAxes, weight='bold', bbox={"boxstyle" : "circle", "lw":0.67, "facecolor":"white", "edgecolor":"black"})
 
-        # - COLORBAR - #
-        cbar = fig.colorbar(pcolor0, ax=axes[-1,0], location='bottom', ticks=clev_l, fraction=1, shrink=0.9, aspect=25, extend='both') # aspect=30
-        cbar.set_label(r"$T'$ / K")
+            # - COLORBAR - #
+            cbar = fig.colorbar(pcolor0, ax=axes[k+1,0], location='bottom', ticks=clev_l, fraction=1, shrink=0.9, aspect=25, extend='both') # aspect=30
+            cbar.set_label(cbar_l)
 
         axes[0,0].set_ylim(zrange[0],zrange[1])
     
@@ -263,7 +263,7 @@ def plot_lidar_filter(CONFIG_FILE):
             duration_str = duration_str + str(int(minutes))
         duration_str = duration_str + 'min'
         fig_name = file_name[:14] + duration_str + '.png'
-        fig.savefig(os.path.join(config.get("OUTPUT","FOLDER"),"filter1",fig_name), facecolor='w', edgecolor='w', format='png', dpi=150, bbox_inches='tight') # orientation='portrait'
+        fig.savefig(os.path.join(config.get("OUTPUT","FOLDER"),'tmp',fig_name), facecolor='w', edgecolor='w', format='png', dpi=150, bbox_inches='tight') # orientation='portrait'
 
         ii += 1 
     return
@@ -271,4 +271,4 @@ def plot_lidar_filter(CONFIG_FILE):
 
 if __name__ == '__main__':
     """provide ini file as argument and pass it to function"""
-    plot_lidar_filter(sys.argv[1])
+    plot_lidar_tmp(sys.argv[1])
